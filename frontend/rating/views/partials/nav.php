659b9a3a2d86f7a569d378dbc8195901b1f0d567
definaru <defina.inc@yandex.ru>
2024-03-27 14:22:41 +0300

    first commit

--- frontend/rating/views/partials/nav.php
+++ frontend/rating/views/partials/nav.php
@@ -1,4 +1,5 @@
 <?php 
+use \yii\widgets\ActiveForm;
 $model = new frontend\rating\forms\SearchForm();
 ?>
 
@@ -9,16 +10,29 @@ $model = new frontend\rating\forms\SearchForm();
                 <div class="col-xs-8 col-sm-10 col-xs-offset-2 col-sm-offset-1">
                     <div class="navbar">
                         <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                                    data-target="#mainmenu"><span class="sr-only">Toggle navigation</span>
+                            <button 
+                                type="button" 
+                                class="navbar-toggle collapsed" 
+                                data-toggle="collapse"
+                                data-target="#mainmenu"
+                            >
+                                <span class="sr-only">Toggle navigation</span>
                                 Навигация
                             </button>
                         </div>
                         <div class="collapse navbar-collapse" id="mainmenu">
                             <ul class="nav navbar-nav">
                                 <li>
-                                    <a href="/o-rejtinge/" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                       aria-haspopup="true" aria-expanded="false">О рейтинге</a>
+                                    <a 
+                                        href="/o-rejtinge/" 
+                                        class="dropdown-toggle" 
+                                        data-toggle="dropdown" 
+                                        role="button"
+                                        aria-haspopup="true" 
+                                        aria-expanded="false"
+                                    >
+                                       О рейтинге
+                                    </a>
                                     <ul class="dropdown-menu">
                                         <li><a href="/o-rejtinge/istoriya/">История</a></li>
                                         <li role="divider">
@@ -33,10 +47,9 @@ $model = new frontend\rating\forms\SearchForm();
                                         <li><a href="/o-rejtinge/instruction/">Инструкции</a></li>
                                         <li role="divider">
                                         <li><a href="/o-rejtinge/konkurs/">Конкурс «БЛОГуч-2023»</a></li>
-
                                     </ul>
                                 </li>
-                                <li><a href="/kriterii/">Критерии</a></li>
+                                <li><a href="/criteria/">Критерии</a></li>
                                 <li><a href="/uchastniki/">Участники</a></li>
                                 <li><a href="/rezults/">Результаты</a></li>
                                 <li><a href="/kontakty/">Контакты</a></li>
@@ -49,18 +62,26 @@ $model = new frontend\rating\forms\SearchForm();
             <?=$this->render('nav/user_nav')?>
 
             <div class="btn-search">
-                <button type="button" id="" data-toggle="collapse" data-target="#searchform" aria-expanded="false"
-                        aria-controls="searchform" class="btn-white btn-round"><i class="fa fa-search"></i></button>
+                <button 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#searchform" 
+                    aria-expanded="false"
+                    aria-controls="searchform" 
+                    class="btn-white btn-round"
+                >
+                    <i class="fa fa-search"></i>
+                </button>
             </div>
             <div id="searchform" class="collapse">
-                <?php $form = \yii\widgets\ActiveForm::begin([
+                <?php $form = ActiveForm::begin([
                     'id' => 'searh-form',
                     'action' => '/search/search_do/',
                     'method' => 'get',
                     'options' => ['class' => 'form-horizontal'],
                 ]) ?>
                     <?= $form->field($model, 'query')->label(false); ?>
-                <?php \yii\widgets\ActiveForm::end() ?>
+                <?php ActiveForm::end() ?>
             </div>
         </div>
     </div>
